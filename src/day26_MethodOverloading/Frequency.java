c0cb696b2c3c5630e7761005f8d7cdf6d9cb2ee2
AB1974 <64151467+AB1974@users.noreply.github.com>
2020-04-22 12:07:46 -0400

    Update Frequency.java

--- src/day26_MethodOverloading/Frequency.java
+++ src/day26_MethodOverloading/Frequency.java
@@ -5,6 +5,7 @@ public class Frequency {
     /*
     Aylin Bekem
     Engin Bekem
+    Arin Bekem
 	1. write a return method called frequency that accepts two parameters: string str and char ch,
 		the method returns the frequency of the ch from the str as an int
 			Ex:
